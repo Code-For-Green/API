66f3d39734eb9414ad90ca6c44c95132d473c733
Rozumek29 <wyrozumialskikacper@gmail.com>
2021-09-20 23:33:31 +0200

    initialCommit

---
+++ src/main/java/com/github/rozumek29/cfg_api/CfgApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.rozumek29.cfg_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfgApiApplication.class, args);
+    }
+
+}
